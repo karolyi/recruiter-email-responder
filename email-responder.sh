8493e3c20623a339a7feed300d4ed2e9e0edae7b
László Károlyi <laszlo@karolyi.hu>
2019-01-22 21:33:55 +0100

    Use sh instead of bash

--- email-responder.sh
+++ email-responder.sh
@@ -1,9 +1,9 @@
-#!/usr/local/bin/bash
+#!/usr/bin/env sh
 
-PROJECT_DIR=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
+PROJECT_DIR=$(cd $(dirname $(readlink -f "$0"));pwd)
 
 cd $PROJECT_DIR
 
-source virtualenv/bin/activate
+. venv/bin/activate
 
-exec python3 email-responder.py
+exec python email-responder.py
